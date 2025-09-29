23337a372d2d110cb882325c06c63f782a6a5bcc
Winston Nguyen <149917453+nwins372@users.noreply.github.com>
2025-09-28 17:02:29 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+<h1 className="text-5xl text-gray-900 dark:text-white">TailwiSSSnd works!</h1>
+
+       
+    
+  )
+}
+
+
